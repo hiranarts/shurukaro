0a8a4aa91a58f8d0478b273389e750bd86624a92
Ali Hamdani <alihamdani@Alis-MBP.attlocal.net>
2024-06-15 15:51:28 -0500

    Fixed makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 CXX:=clang++
-SDL_INCLUDE_DIR:=-Ilib/SDL2.framework/Headers/ -Iinclude/
+SDL_INCLUDE_DIR:=-Ilib/SDL2.framework/Headers/ 
+INCLUDE_DIR:=-Iinclude/
+LIB_DIR:=-Flib/
 SDL_LIB_DIR:=-Flib/ -framework SDL2
+SDL_DLL:= -rpath ../lib/
 SRC_FILES:=src/platform.cpp
+CFLAGS:=-Wall -Werror
+ASAN_FLAGS:=-fsanitize=address
+OBJ_DIR:=bin/
 
-app: main.o platform.o
-	$(CXX) main.o platform.o -o bin/app
-main.o: main.cpp
-	$(CXX) -c main.cpp $(INCLUDE_DIR) -Flib/ -framework SDL2 -rpath ../lib/ -o main.o
-platform.o: src/platform.cpp include/platform.h
-	$(CXX) -c src/platform.cpp $(SDL_INCLUDE_DIR) -o platform.o
+app: main.cpp $(OBJ_DIR)platform.o
+	$(CXX) $(CFLAGS) $(ASAN_FLAGS) main.cpp $(OBJ_DIR)platform.o $(INCLUDE_DIR) $(SDL_LIB_DIR) $(SDL_DLL) -o bin/app
+$(OBJ_DIR)platform.o: src/platform.cpp include/platform.h
+	$(CXX) -c src/platform.cpp $(INCLUDE_DIR) $(LIB_DIR) -o $(OBJ_DIR)platform.o
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <platform.h>
 #include <stdio.h>
 
 #define SCREEN_WIDTH 1280 
@@ -10,13 +11,13 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if(!window){
+    platform::WINDOW = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(!platform::WINDOW){
         printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(platform::WINDOW, -1, SDL_RENDERER_ACCELERATED);
     if(!renderer){
         printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
         return 1;
