e39d08db803653d74522d5aceade9697ece55b66
Ali Hamdani <alihamdani@Alis-MBP.attlocal.net>
2024-06-27 16:46:35 -0500

    adding thread functionality

--- src/platform.cpp
+++ src/platform.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2_mixer/SDL_mixer.h>
+#include <stdlib.h>
 
 SDL_Window* WINDOW;
 SDL_Renderer* RENDERER;
@@ -7,15 +8,23 @@ int SDLFLAGS = SDL_INIT_VIDEO;
 unsigned int SCREEN_WIDTH = 1280;
 unsigned int SCREEN_HEIGHT = 720;
 int WINDOWFLAGS = SDL_WINDOW_SHOWN|SDL_WINDOW_ALLOW_HIGHDPI;
-const char data_path[] = "../data/";
+int MIXERFLAGS = MIX_DEFAULT_FORMAT;
+char field_buffer[200];
+Mix_Music* testMusic;
+unsigned int W_ASPECT_RATIO = 3;
+unsigned int H_ASPECT_RATIO = 2;
+unsigned int PIXELS = 240;
+unsigned int GAME_FRAME_RATE = 60;
+
+const char* data_path = "../data/";
 int platformInit(){
     //SDL init
     if( SDL_Init(SDLFLAGS) < 0 ){
-	printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	return -1;
+	  printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+	  return -1;
     }
     //Window init
-    WINDOW = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    WINDOW = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, W_ASPECT_RATIO * PIXELS, H_ASPECT_RATIO * PIXELS, SDL_WINDOW_SHOWN );
     if (!WINDOW){
 	printf("SDL Window could not be created! SDL_Error: %s\n", SDL_GetError());
 	return -1;
@@ -26,7 +35,7 @@ int platformInit(){
       printf("SDL Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
       return -1;
     }
-	//For high dpi mode and 
+	//For high dpi mode macos specifically
 	int rw = 0, rh = 0;
 	SDL_GetRendererOutputSize(RENDERER, &rw, &rh);
 	if (rw != SCREEN_WIDTH || rh != SCREEN_HEIGHT) {
@@ -41,10 +50,36 @@ int platformInit(){
 		SDL_RenderSetScale(RENDERER, widthScale, heightScale);
 	}
 	/*SDL Mixer initialization */
-	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+	if (Mix_OpenAudio(44100, MIXERFLAGS, 2, 2048) < 0) {
 		printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
 		return -2;
 	}
-
     return 0;
+}
+
+int platformDestroy(){
+  SDL_Quit();
+  Mix_Quit();
+  SDL_DestroyWindow(WINDOW);
+  SDL_DestroyRenderer(RENDERER);
+  WINDOW = NULL;
+  RENDERER = NULL;
+  return 0;
+}
+/* a concat function for c style strings, this allocates memory */ 
+char* concat(const char* s1, const char* s2){
+  printf("size allocation %lu\n", strlen(s1) + strlen(s2) + 1);
+  char* result = (char*)calloc(1, strlen(s1) + strlen(s2) + 1);
+  strcpy(result, s1);
+  strcat(result,s2);
+  printf("memory allocated size %lu\n", strlen(result));
+  return result;
+}
+/* This code loads a music file from the data path */
+Mix_Music* platformLoadMusic(const char* filepath){
+  char* freeMe = concat(data_path, filepath);
+  printf("Loading Track: %s",freeMe);
+  Mix_Music* musicFilePtr = Mix_LoadMUS(freeMe);
+  free(freeMe);
+  return musicFilePtr;
 }
